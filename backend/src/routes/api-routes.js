c36635a35f78ac4dff7e2787103262e3bd481eb4
Alexander Tewes <alexandertewes7@gmail.com>
2021-11-19 21:46:41 +0100

    add salesman routes (create, read, update)

--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -1,9 +1,10 @@
+
 const express = require('express');
 const router = express.Router();
 const {checkAuthorization} = require('../middlewares/auth-middleware');
 
 /*
-    In this file is the routing for the REST-endpoints under /api managed
+    In this file the routing for the REST-endpoints under /api is managed
  */
 
 const authApi = require('../apis/auth-api'); //api-endpoints are loaded from separate files
@@ -13,5 +14,12 @@ router.get('/login', authApi.isLoggedIn); //the function, which handles requests
 
 const userApi = require('../apis/user-api');
 router.get('/user', checkAuthorization(), userApi.getSelf);
+
+const salesmanApi = require('../apis/salesman-api');
+router.post('/salesman', salesmanApi.create);
+router.get('/salesman/:sid', salesmanApi.findBySid);
+router.get('/find-all', salesmanApi.findAll);
+router.put('/salesman/:sid', salesmanApi.update);
+
 
 module.exports = router;
