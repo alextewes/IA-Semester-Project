9e3c85f3ec8dfb592e268a16b1e3bbf147bf907b
Alexander Tewes <alexandertewes7@gmail.com>
2021-11-19 22:28:28 +0100

    create mongoose schema for performance records

--- backend/src/models/PerformanceRecord.model.js
+++ backend/src/models/PerformanceRecord.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const PerformanceRecordSchema = mongoose.Schema({
-    erid: Number,
+    prid: Number,
     actualValue: Number,
     targetValue: Number,
     year: Number,
