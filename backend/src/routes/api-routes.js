82cce9f9efaebb6a0614e293a5142a86ee109600
Alexander Tewes <alexandertewes7@gmail.com>
2021-11-19 22:27:51 +0100

    update api routes (crud performance records)

--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -18,8 +18,16 @@ router.get('/user', checkAuthorization(), userApi.getSelf);
 const salesmanApi = require('../apis/salesman-api');
 router.post('/salesman', salesmanApi.create);
 router.get('/salesman/:sid', salesmanApi.findBySid);
-router.get('/find-all', salesmanApi.findAll);
+router.get('/salesman', salesmanApi.findAll);
 router.put('/salesman/:sid', salesmanApi.update);
+router.delete('/salesman/:sid', salesmanApi.delete);
+
+const performanceRecordApi = require('../apis/performance-record-api');
+router.post('/performance-record', performanceRecordApi.create);
+router.get('/performance-record/:prid', performanceRecordApi.findBySid);
+router.get('/performance-record', performanceRecordApi.findAll);
+router.put('/performance-record/:prid', performanceRecordApi.update);
+router.delete('/performance-record/:prid', performanceRecordApi.delete);
 
 
 module.exports = router;
