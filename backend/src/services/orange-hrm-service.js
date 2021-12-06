fea5f6860dee8bc83c39e5d9c4d663025d053219
Jan Frericks <jan.frericks@smail.inf.h-brs.de>
2021-12-06 16:42:03 +0100

    First Integration of OrangeHRM

--- backend/src/services/orange-hrm-service.js
+++ backend/src/services/orange-hrm-service.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const qs = require('querystring');
-
+const Salesman = require('../models/Salesman.model')
 const baseUrl = 'https://sepp-hrm.inf.h-brs.de/symfony/web/index.php';
+
 let accessToken = null;
 const body = qs.stringify({
     client_id: 'api_oauth_id',
@@ -16,7 +17,8 @@ const config = {
         'Accept': 'application/json',
     }
 };
-export async function getOrangeHrmToken() {
+
+getOrangeHrmToken = async () => {
     const res = await axios.post(`${baseUrl}/oauth/issueToken`, body, config);
     if (res.data.error) {
         throw Error(res.data.error);
@@ -24,3 +26,27 @@ export async function getOrangeHrmToken() {
     accessToken = res.data['access_token'];
     return accessToken;
 }
+
+getAllEmployees = async () => {
+    let token = await getOrangeHrmToken()
+    let config = {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Accept': 'application/json'
+        }
+    };
+    const accounts = await axios.get('https://sepp-hrm.inf.h-brs.de/symfony/web/index.php/api/v1/employee/search', config);
+    const unclearedAccounts = accounts.data.data;
+    let employees = []
+    for(let e of unclearedAccounts){
+        const firstName = e.firstName;
+        const lastName = e.lastName;
+        const dob = e.dob;
+        const department = e.unit;
+        let employee = new Salesman({undefined,firstName,lastName, dob, department});
+        employees.push(employee);
+    }
+    console.log(employees)
+    return employees;
+}
+getAllEmployees()
