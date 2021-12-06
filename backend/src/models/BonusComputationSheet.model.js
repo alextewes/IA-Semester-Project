c8deec68202426a31321e796bbc2eea7b637ed93
Alexander Tewes <alexandertewes7@gmail.com>
2021-12-06 15:46:10 +0100

    implemenet BonusComputationSheet model

---
+++ backend/src/models/BonusComputationSheet.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Salesorder = require('Salesorder.model');
+const PerformanceRecord = require('PerformanceRecord.model');
+
+const BonusComputationSheetSchema = mongoose.Schema({
+    bcid: Number,
+    sid: Number,
+    salesorders: [Salesorder],
+    performanceRecord: PerformanceRecord
+});
+
+module.exports = mongoose.model('BonusComputationSheet', BonusComputationSheetSchema);
