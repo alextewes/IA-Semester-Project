01bd76bc3dc73ec412449e48dc89ea94145c99f0
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2021-12-06 19:45:49 +0100

    Added Ag grid imports Added MatImports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { ExamplePageComponent } from './pages/example-page/example-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { BonusComputationPageComponent } from './pages/bonus-computation-page/bonus-computation-page.component';
+import {AgGridModule} from 'ag-grid-angular';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { PerformanceRecordsPageComponent } from './pages/performance-records-page/performance-records-page.component';
+import { SalesmenPageComponent } from './pages/salesmen-page/salesmen-page.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +35,9 @@ import { BonusComputationPageComponent } from './pages/bonus-computation-page/bo
     ExamplePageComponent,
     NotFoundPageComponent,
     FooterBarComponent,
-    BonusComputationPageComponent
+    BonusComputationPageComponent,
+    PerformanceRecordsPageComponent,
+    SalesmenPageComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +49,11 @@ import { BonusComputationPageComponent } from './pages/bonus-computation-page/bo
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    AgGridModule.withComponents([]),
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
