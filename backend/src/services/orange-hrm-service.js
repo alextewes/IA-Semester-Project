4eb2387d82c680b08d81dba52f2607486633e30b
Alexander Tewes <alexandertewes7@gmail.com>
2021-12-06 12:17:42 +0100

    implement orange-hrm connection and bearer auth

---
+++ backend/src/services/orange-hrm-service.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const qs = require('querystring');
+
+const baseUrl = 'https://sepp-hrm.inf.h-brs.de/symfony/web/index.php';
+let accessToken = null;
+const body = qs.stringify({
+    client_id: 'api_oauth_id',
+    client_secret: 'oauth_secret',
+    grant_type: 'password',
+    username: 'demouser',
+    password: '*Safb02da42Demo$'
+});
+const config = {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+    }
+};
+export async function getOrangeHrmToken() {
+    const res = await axios.post(`${baseUrl}/oauth/issueToken`, body, config);
+    if (res.data.error) {
+        throw Error(res.data.error);
+    }
+    accessToken = res.data['access_token'];
+    return accessToken;
+}
