c9aa74c47f71dd59c330b1001674720f0cbf806c
Jan Frericks <jan.frericks@smail.inf.h-brs.de>
2021-12-13 18:14:52 +0100

    Implemenation of a simple SwaggerUI with swagger-ui-express

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,10 @@ const upload = multer();
 const app = express();
 const crypto = require('crypto');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
