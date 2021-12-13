a7e0904cc8505a2f4864b2235659321294deec0f
Alexander Tewes <alexandertewes7@gmail.com>
2021-12-13 21:24:18 +0100

    implement integration of opencrx for salesorders

--- backend/src/services/open-crx-service.js
+++ backend/src/services/open-crx-service.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const Salesorder = require("../models/Salesorder.model")
+const mongoose = require("mongoose");
 
 const baseUrl = 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX';
 const credentials = {
@@ -12,18 +14,62 @@ const config = {
     },
     auth: credentials,
 };
-async function fetchCustomers() {
-    const contacts = await axios.get(`${baseUrl}/org.opencrx.kernel.account1/provider/CRX/segment/Standard/account`, config);
-    const customers = contacts.data.objects;
-    //console.log(customers)
-    return customers;
-}
 
 async function fetchSalesOrders() {
-    const sales = await axios.get(`${baseUrl}/org.opencrx.kernel.contract1/provider/CRX/segment/Standard/salesOrder`, config)
-    const salesorders = sales.data.objects;
-    return salesorders;
+    try {
+        const sales = await axios.get(`${baseUrl}/org.opencrx.kernel.contract1/provider/CRX/segment/Standard/salesOrder`, config)
+        return sales.data.objects;
+    }
+    catch(e) {
+        console.log(e);
+    }
 }
+
+const createSalesOrders = async function() {
+    try {
+        const salesorders = await fetchSalesOrders();
+        for(const salesorder of salesorders) {
+            const salesRep = await axios.get(salesorder.salesRep['@href'], config);
+            const customer = await axios.get(salesorder.customer['@href'], config);
+            const positionResponse = await axios.get(salesorder['@href'] + '/position', config);
+            if(positionResponse.data.objects !== undefined) {
+                const position = positionResponse.data.objects[0];
+                const id = mongoose.Types.ObjectId(salesorder['@href'].split('/').pop().slice(0,12));
+                const sid = salesRep.data.governmentId;
+                const year = parseInt(salesorder.createdAt.slice(0, 4));
+                const product = position.productDescription === "Hoover for big companies" ? "HooverGo" : "HooverClean";
+                const customerName = customer.data.fullName;
+                const clientRanking = customer.data.accountRating;
+                const items = parseInt(position.quantity);
+                const bonus = 0;
+                const query = {
+                    _id: id,
+                    sid: sid,
+                    year: year,
+                    product: product,
+                    customerName: customerName,
+                    clientRanking: clientRanking,
+                    items: items,
+                    bonus: bonus
+                }
+                if(await Salesorder.exists({_id: id})) {
+                    await Salesorder.findByIdAndUpdate(id, query);
+                }
+                else {
+                    await new Salesorder(query).save();
+                }
+            }
+        }
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+module.exports = {
+    createSalesOrders
+}
+
 
 
 
