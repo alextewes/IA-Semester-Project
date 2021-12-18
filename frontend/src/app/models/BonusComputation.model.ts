2378912a7a3106dcabf96af579f38013f600e1ae
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2021-12-18 16:11:10 +0100

    Changed performanceRecords entry to string array to accommodate mongodb objectids.

--- frontend/src/app/models/BonusComputation.model.ts
+++ frontend/src/app/models/BonusComputation.model.ts
@@ -1,10 +1,8 @@
-import {PerformanceRecord} from './PerformanceRecord.model';
-
 export interface BonusComputation{
   _id: string;
   sid: number;
   year: number;
   value: number;
-  performanceRecords: PerformanceRecord[];
-  salesOrders: string;
+  performanceRecords: string[];
+  salesOrders: string[];
 }
