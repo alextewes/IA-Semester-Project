7fb03862c25431e3726d5c667ea5e3fecc9f39b4
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2022-01-18 09:33:34 +0100

    Added put method

--- frontend/src/app/services/performance-record.service.ts
+++ frontend/src/app/services/performance-record.service.ts
@@ -21,4 +21,7 @@ export class PerformanceRecordService {
   public postPeformanceRecord(performanceRecord: PerformanceRecord): Observable<PerformanceRecord>{
     return this.httpClient.post<PerformanceRecord>(this.baseUrl, performanceRecord);
   }
+  public putPerformanceRecord(prid: string, performanceRecord: PerformanceRecord): Observable<PerformanceRecord>{
+    return this.httpClient.put<PerformanceRecord>(this.baseUrl + '/' + prid, performanceRecord);
+  }
 }
