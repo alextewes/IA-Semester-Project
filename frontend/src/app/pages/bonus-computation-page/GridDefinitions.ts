3159ee2dd286e5613203cc9defde20e5f5adfd3a
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2022-02-07 17:57:45 +0100

    Extracted performance row data for empty entries. Extracted Salesmen column definitions.

---
+++ frontend/src/app/pages/bonus-computation-page/GridDefinitions.ts
@@ -0,0 +1,17 @@
+export const performanceRowDataEmpty = [
+  {goalDesc: 'Leadership Competence', targetValue: 0, actualValue: 0, bonus: 0, sid: 0},
+  {goalDesc: 'Openness to Employee', targetValue: 0, actualValue: 0, bonus: 0, sid: 0},
+  {goalDesc: 'Social Behaviour to Employee', targetValue: 0, actualValue: 0, bonus: 0, sid: 0},
+  {goalDesc: 'Attitude towards Client', targetValue: 0, actualValue: 0, bonus: 0, sid: 0},
+  {goalDesc: 'Communication Skills', targetValue: 0, actualValue: 0, bonus: 0, sid: 0},
+  {goalDesc: 'Integrity to Company', targetValue: 0, actualValue: 0, bonus: 0, sid: 0}
+];
+
+export const salesmenColumnDefs = [
+  {field: '_id', headerName: 'SID', sortable: true, flex: 1},
+  {field: 'employeeId', headerName: 'EmployeeId', sortable: true, flex: 1},
+  {field: 'firstName', headerName: 'First Name', sortable: true, flex: 1},
+  {field: 'lastName', headerName: 'Last Name', sortable: true, flex: 1},
+  {field: 'dob', headerName: 'Date of Birth', sortable: true, flex: 1},
+  {field: 'department', headerName: 'Department', sortable: true, flex: 1},
+];
